296337409ffeb0fd01a95a2c7250919b69654aeb
Abhinay Khoparzi <abhinayk@gmail.com>
2015-05-14 19:17:32 +0100

    Add month separators to events

--- home.php
+++ home.php
@@ -14,6 +14,42 @@
  * @since WP-Forge 5.5.1.8
  */
 
+	function khoparzi_events_list_the_date_headers() {
+
+		/* Month and year separators (on every month and year change) */
+
+		$show_headers = apply_filters( 'tribe_events_list_show_date_headers', true );
+
+		$html = '';
+
+		if ( $show_headers ) {
+
+			global $post, $wp_query;
+
+			$event_year        = tribe_get_start_date( $post, false, 'Y' );
+			$event_month       = tribe_get_start_date( $post, false, 'm' );
+			$month_year_format = tribe_get_option( 'monthAndYearFormat', 'F Y' );
+
+			if ( $wp_query->current_post > 0 ) {
+				$prev_post        = $wp_query->posts[$wp_query->current_post - 1];
+				$prev_event_year  = tribe_get_start_date( $prev_post, false, 'Y' );
+				$prev_event_month = tribe_get_start_date( $prev_post, false, 'm' );
+			}
+
+
+			/*
+			 * If the event month changed since the last event in the loop,
+			 * or is the same month but the year changed.
+			 *
+			 */
+			if ( $wp_query->current_post === 0 || ( $prev_event_month != $event_month || ( $prev_event_month == $event_month && $prev_event_year != $event_year ) ) ) {
+				$html .= sprintf( "<li class='box monthYear'><a class='dateLink'>%s</a></li>", tribe_get_start_date( $post, false, $month_year_format ) );
+			}
+
+			echo apply_filters( 'tribe_events_list_the_date_headers', $html, $event_month, $event_year );
+		}
+	}
+
 get_header(); ?>
 
 	<div id="content" class="medium-12 large-12 columns" role="main">
@@ -44,6 +80,11 @@ get_header(); ?>
 
 		<ul class="block date-list">
 		<?php while ( $events->have_posts() ) : $events->the_post(); ?>
+
+			<!-- Month / Year Headers -->
+			<?php khoparzi_events_list_the_date_headers(); ?>
+
+			<!-- Day box -->
 			<li class="box">
 				<a class="url" href="<?php echo esc_url( tribe_get_event_link() ); ?>" title="<?php the_title() ?>" rel="bookmark">
 					<?php the_title() ?>
